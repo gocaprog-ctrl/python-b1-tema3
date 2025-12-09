ea6b9bb1eb8148de780e0d4fc1313df74761fd3c
gocaprog-ctrl <gocaprog@gmail.com>
2025-12-09 17:31:57 +0100

    ej3a1_operations.py
    Implemented basic arithmetic operations in the functions.

--- 3a/ej3a1/ej3a1_operations.py
+++ 3a/ej3a1/ej3a1_operations.py
@@ -2,20 +2,20 @@
 
 
 def add(num_1: float, num_2: float) -> float:
-    # Write here your code
+    num_1 + num_2
     pass
 
 
 def subtract(num_1: float, num_2: float) -> float:
-    # Write here your code
+    num_1 - num_2
     pass
 
 
 def multiply(num_1: float, num_2: float) -> float:
-    # Write here your code
+    num_1 * num_2
     pass
 
 
 def divide(num_1: float, num_2: float) -> float:
-    # Write here your code
+    num_1 / num_2
     pass
